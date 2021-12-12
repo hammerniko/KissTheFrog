31f83a51065e3b385479b97424e02fdd99a06675
hammerniko <hammer.nikolai@googlemail.com>
2021-12-12 11:26:47 +0100

    Dunkeler Hintergrund eingestellt

--- app/src/main/java/de/hgs/kissthefrog/MainActivity.java
+++ app/src/main/java/de/hgs/kissthefrog/MainActivity.java
@@ -2,12 +2,17 @@ package de.hgs.kissthefrog;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+
         setContentView(R.layout.activity_main);
     }
 }
