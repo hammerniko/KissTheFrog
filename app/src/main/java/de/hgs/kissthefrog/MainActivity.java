e8dc862dc24af50b720203a548cabdf172447940
hammerniko <hammer.nikolai@googlemail.com>
2021-12-27 13:36:00 +0100

    Eigene View added

--- app/src/main/java/de/hgs/kissthefrog/MainActivity.java
+++ app/src/main/java/de/hgs/kissthefrog/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         setContentView(R.layout.activity_main);
         showStartFragment();
@@ -31,13 +30,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initRound() {
         countdown = 10;
+        ViewGroup container = (ViewGroup) findViewById(R.id.container);
+        container.removeAllViews();
+        WimmelView wv = new WimmelView(this);
+        container.addView(wv, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        wv.setImageCount(8*(10+round));
         update();
     }
 
     private void fillTextView(int id, String text){
         TextView tv = (TextView) findViewById(id);
         tv.setText(text);
-
     }
 
     private void update(){
@@ -63,9 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.tvStart){
-            //startGame();
-
-            showGameOverFragment();
+            startGame();
             Toast.makeText(this,"Start",Toast.LENGTH_SHORT).show();
         }
         else if(v.getId() == R.id.tvPlayAgain){
---
+++ app/src/main/java/de/hgs/kissthefrog/WimmelView.java
@@ -0,0 +1,60 @@
+package de.hgs.kissthefrog;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+import java.util.Random;
+
+public class WimmelView extends View {
+
+
+    //Variablen
+    private Paint paint;        //Virtueller Pinsel zum zeichnen
+    private Random rnd;         //Zufällige Pos. für jedes WimmelObjekt
+    private long randomSeed=1;  //Startwert für den Zufallsgenerator
+    private int imageCount;     //Anzahl der zu ladenden Bilder
+
+    //Zu verwendende Bild in Array laden
+    private static final int[] images = {R.mipmap.distract1, R.mipmap.distract2, R.mipmap.distract3, R.mipmap.distract4, R.mipmap.distract5,
+    R.mipmap.distract6,R.mipmap.distract7,R.mipmap.distract8};
+
+
+    public WimmelView(Context context) {
+        super(context);
+        paint.setAntiAlias(true);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        rnd = new Random(randomSeed);
+        for (int image :images){
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), image);
+
+            for (int i=0; i<imageCount/images.length; i++){
+                float left = (float) (rnd.nextFloat()*getWidth()-bitmap.getWidth());
+                float top  = (float) (rnd.nextFloat()*getHeight()-bitmap.getHeight());
+
+                canvas.drawBitmap(bitmap,left,top,paint);
+            }
+            bitmap.recycle();
+        }
+
+
+
+    }
+
+    public void setImageCount(int imageCount){
+        this.imageCount = imageCount;
+        randomSeed = System.currentTimeMillis();
+
+        /**
+         * Kennzeichnet gezeichneten Bereich als veraltet und zeichnet ihn bei
+         * Gelegenheit neu
+         */
+        invalidate();
+    }
+}
