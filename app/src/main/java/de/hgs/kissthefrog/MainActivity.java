278918095cd02c7da933a091007730f192aaafa8
hammerniko <hammer.nikolai@googlemail.com>
2021-12-27 16:00:38 +0100

    Spiel spielbar ohne Punktanzeige

--- app/src/main/java/de/hgs/kissthefrog/MainActivity.java
+++ app/src/main/java/de/hgs/kissthefrog/MainActivity.java
@@ -1,15 +1,33 @@
 package de.hgs.kissthefrog;
 
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            countdown();
+        }
+    };
+
+
+
+
+    private static final int FROG_ID = 212121;
+    private ImageView frog;
     private int points;
     private int round;
     private int countdown;
@@ -17,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView tvPoints;
     private TextView tvRound;
     private TextView tvCountdown;
+    private Random rnd = new Random();
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +59,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initRound() {
         countdown = 10;
+
         ViewGroup container = (ViewGroup) findViewById(R.id.container);
         container.removeAllViews();
         WimmelView wv = new WimmelView(this);
         container.addView(wv, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         wv.setImageCount(8*(10+round));
+
+
+        frog = new ImageView(this);
+        frog.setId(FROG_ID);
+        frog.setImageResource(R.mipmap.frog);
+        frog.setScaleType(ImageView.ScaleType.CENTER);
+        float scale = getResources().getDisplayMetrics().density;
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(Math.round(64*scale), Math.round(61*scale));
+        lp.gravity = Gravity.TOP+Gravity.LEFT;
+        lp.leftMargin = rnd.nextInt(container.getWidth()-64);
+        lp.topMargin =rnd.nextInt(container.getHeight()-61);
+        frog.setOnClickListener(this);
+        container.addView(frog,lp);
+
         update();
+        handler.postDelayed(runnable, 1000-round*50);
     }
 
 
@@ -77,10 +113,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if(v.getId() == R.id.tvPlayAgain){
             showStartFragment();
          }
+        else if (v.getId()==FROG_ID){
+            handler.removeCallbacks(runnable);
+            Toast.makeText(this, R.string.kissed, Toast.LENGTH_SHORT).show();
+            points+=countdown*1000;
+            round++;
+            initRound();
+        }
     }
 
     private void startGame() {
 
         newGame();
+    }
+
+
+    private void countdown(){
+        countdown--;
+        update();
+        if (countdown<=0){
+            frog.setOnClickListener(null);
+            showGameOverFragment();
+        }
+        else{
+            handler.postDelayed(runnable, 1000-round*50);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
     }
 }
